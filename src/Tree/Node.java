58244ae7dc0b62e7c33d2d434441c4396290b5ea
Muhamad Salah <mohamedsalaaa7@gmail.com>
2020-04-09 18:50:25 +0200

    testing

--- src/Tree/Node.java
+++ src/Tree/Node.java
@@ -3,34 +3,40 @@ package Tree;
 import Tree.INode;
 
 public class Node implements INode {
+    INode leftChild=null,rightChild=null,parent=null;
+    Object value;
+    boolean color=BLACK;
     @Override
     public void setParent(INode parent) {
-
+    this.parent=parent;
+    if(parent.getColor()==RED){
+        this.setColor(BLACK);
+    }
     }
 
     @Override
     public INode getParent() {
-        return null;
+        return this.parent;
     }
 
     @Override
     public void setLeftChild(INode leftChild) {
-
+    this.leftChild=leftChild;
     }
 
     @Override
     public INode getLeftChild() {
-        return null;
+        return this.leftChild;
     }
 
     @Override
     public void setRightChild(INode rightChild) {
-
+    this.rightChild=rightChild;
     }
 
     @Override
     public INode getRightChild() {
-        return null;
+        return this.rightChild;
     }
 
     @Override
@@ -45,26 +51,39 @@ public class Node implements INode {
 
     @Override
     public Object getValue() {
-        return null;
+        return this.value;
     }
 
     @Override
     public void setValue(Object value) {
-
+    this.value=value;
     }
 
     @Override
     public boolean getColor() {
-        return false;
+        return this.color;
+
     }
 
     @Override
     public void setColor(boolean color) {
-
+    if (color=true){
+        this.color=RED;
+    }
+    else{
+        this.color=BLACK;
+    }
     }
 
     @Override
     public boolean isNull() {
         return false;
+    }
+
+    public static void main(String[] args) {
+        INode x=new Node();
+        INode y=new Node();
+        x.setParent(y);
+        System.out.printf("%s %s %s ",y.getLeftChild(),y.getRightChild(),x.getParent());
     }
 }
